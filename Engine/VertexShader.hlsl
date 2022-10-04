7f9456f77fb8ced15ec9ebc0e47168b18185a531
joshuaridsdill <joshuaridsdill@outlook.com>
2022-10-04 15:19:44 +0100

    Changes to the shaders!

--- Engine/VertexShader.hlsl
+++ Engine/VertexShader.hlsl
@@ -1,8 +1,12 @@
 #version 330 core
 
 layout(location = 0) in vec4 position;
+layout(location = 0) in vec3 aColor;
+
+out vec3 outColor;
 
 void main()
 {
-   gl_Position = position;
+   gl_Position = vec4(position, 1.0);
+   ourColor = aColor;
 };
